d9b08b35f60ccdd5edce502e1c2b92a948322b8e
elialgranti <elialgranti@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-12-20 01:28:57 +0000

    tagging version 1.0.0
    git-svn-id: https://plugins.svn.wordpress.org/geoswitch/tags/1.0.0@1049939 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ class.geoswitch.php
@@ -0,0 +1,188 @@
+<?php
+if ( ! defined( 'ABSPATH' ) )
+        die( 'This is just a Wordpress plugin.' );
+ 
+if ( ! defined( 'GEOSWITCH_PLUGIN_DIR' ) )
+    define( 'GEOSWITCH_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
+require_once GEOSWITCH_PLUGIN_DIR . 'vendor/autoload.php';
+
+class GeoSwitch {
+    private static $initialized = false;
+    private static $user_ip = null;
+    private static $record = null;
+    private static $useKm = true;
+	
+	public static function init() {
+        if (self::$initialized) {
+            return;
+        }
+        self::$initialized = true;
+        
+        self::$user_ip = self::get_user_ip();
+
+        try {
+            $opt = get_option('geoswitch_options');
+            $useKM = ($opt['units'] == 'km');
+            $database = GEOSWITCH_PLUGIN_DIR . 'database/' . $opt['database_name'];
+            $reader = new GeoIp2\Database\Reader($database);
+
+            self::$record = $reader->city(self::$user_ip);
+        } catch (Exception $e) {
+            self::$record = null;
+        }
+
+        add_shortcode('geoswitch', array( 'GeoSwitch', 'switch_block' ));
+        add_shortcode('geoswitch_case', array( 'GeoSwitch', 'switch_case' ));
+        
+        add_shortcode('geoswitch_ip', array( 'GeoSwitch', 'get_ip' ));
+        add_shortcode('geoswitch_city', array( 'GeoSwitch', 'get_city' ));
+        add_shortcode('geoswitch_state', array( 'GeoSwitch', 'get_state' ));
+        add_shortcode('geoswitch_state_code', array( 'GeoSwitch', 'get_state_code' ));
+        add_shortcode('geoswitch_country', array( 'GeoSwitch', 'get_country' ));
+        add_shortcode('geoswitch_country_code', array( 'GeoSwitch', 'get_country_code' ));
+        add_shortcode('geoswitch_latitude', array( 'GeoSwitch', 'get_latitude' ));
+        add_shortcode('geoswitch_longitude', array( 'GeoSwitch', 'get_longitude' ));
+    }
+
+	public static function switch_block($atts, $content) {
+		$str = do_shortcode($content);
+        $arr = explode('#', $str, 3);
+        
+        return count($arr) == 3  
+            ? substr($arr[2], 0, intval($arr[1]))
+            : '';
+    }
+
+	public static function switch_case($atts, $content) {
+        $expandedContent = do_shortcode($content);
+        
+        if (is_null(self::$record)) {
+            if (!empty($atts['city']) ||
+                !empty($atts['state']) || 
+                !empty($atts['state_code']) || 
+                !empty($atts['country']) || 
+                !empty($atts['country_code']) ||
+                !empty($atts['within']) ||
+                !empty($atts['from'])) {
+                    return '';
+            }
+            return '#'.strlen($expandedContent).'#'.$expandedContent;
+        }
+        
+        
+        if ((empty($atts['city']) || strcasecmp($atts['city'], self::$record->city->name) == 0)
+            &&
+            (empty($atts['state']) || strcasecmp($atts['state'], self::$record->mostSpecificSubdivision->name) == 0)
+            &&
+            (empty($atts['state_code']) || strcasecmp($atts['state_code'], self::$record->mostSpecificSubdivision->isoCode) == 0)
+            &&
+            (empty($atts['country']) || strcasecmp($atts['country'], self::$record->country->name) == 0)
+            &&
+            (empty($atts['country_code']) || strcasecmp($atts['country_code'], self::$record->country->isoCode) == 0) 
+            &&
+            (empty($atts['within']) || self::within($atts['within'], $atts['from']))) {
+            return '#'.strlen($expandedContent).'#'.$expandedContent;
+        }
+        return '';
+    }
+
+    public static function get_ip($atts, $content) {
+        return self::$user_ip;
+    }
+
+    public static function get_city($atts, $content) {
+        if (is_null(self::$record)) {
+            return '?';
+        }
+        return self::$record->city->name;
+    }
+
+    public static function get_state($atts, $content) {
+        if (is_null(self::$record)) {
+            return '?';
+        }
+        return self::$record->mostSpecificSubdivision->name;
+    }
+
+    public static function get_state_code($atts, $content) {
+        if (is_null(self::$record)) {
+            return '?';
+        }
+        return self::$record->mostSpecificSubdivision->isoCode;
+    }
+    
+    public static function get_country($atts, $content) {
+        if (is_null(self::$record)) {
+            return '?';
+        }
+        return self::$record->country->name;
+    }
+
+    public static function get_country_code($atts, $content) {
+        if (is_null(self::$record)) {
+            return '?';
+        }
+        return self::$record->country->isoCode;
+    }
+
+    public static function get_latitude($atts, $content) {
+        if (is_null(self::$record)) {
+            return '?';
+        }
+        return self::$record->location->latitude;
+    }
+
+    public static function get_longitude($atts, $content) {
+        if (is_null(self::$record)) {
+            return '?';
+        }
+        return self::$record->location->longitude;
+    }
+
+
+    public static function activation() {
+        $default_options=array(
+            'database_name'=>'GeoLite2-City.mmdb',
+            'units'=>'km'
+         );
+        add_option('geoswitch_options',$default_options);
+    }
+    
+    public static function deactivation() {
+        unregister_setting('geoswitch_options', 'geoswitch_options');
+        delete_option('geoswitch_options');
+    }
+    
+    private static function within($within, $from) {
+        $within = 0.0 + $within;
+        $from = explode(',', $from, 2);
+        
+        $pi80 = M_PI / 180;
+        $lat1 = ($from[0] + 0.0) * $pi80;
+        $lng1 = ($from[1] + 0.0) * $pi80;
+        $lat2 = self::$record->location->latitude * $pi80;
+        $lng2 = self::$record->location->longitude * $pi80;
+ 
+        $r = 6372.797; // mean radius of Earth in km
+        $dlat = $lat2 - $lat1;
+        $dlng = $lng2 - $lng1;
+        $a = sin($dlat / 2) * sin($dlat / 2) + cos($lat1) * cos($lat2) * sin($dlng / 2) * sin($dlng / 2);
+        $c = 2 * atan2(sqrt($a), sqrt(1 - $a));
+        $km = $r * $c;
+        
+        return self::$useKm ? ($km <= $within) : (($km * 0.621371192) <= $within);
+    }
+    
+    private static function get_user_ip() {
+        if ( ! empty( $_SERVER['HTTP_CLIENT_IP'] ) ) {
+            //check ip from share internet
+            $ip = $_SERVER['HTTP_CLIENT_IP'];
+        } elseif ( ! empty( $_SERVER['HTTP_X_FORWARDED_FOR'] ) ) {
+            //to check ip is pass from proxy
+            $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+        } else {
+            $ip = $_SERVER['REMOTE_ADDR'];
+        }
+        return $ip;
+    }
+}
---
+++ class.geoswitch_admin.php
@@ -0,0 +1,89 @@
+<?php
+if ( ! defined( 'ABSPATH' ) )
+        die( 'This is just a Wordpress plugin.' );
+ 
+if ( ! defined( 'GEOSWITCH_PLUGIN_DIR' ) )
+    define( 'GEOSWITCH_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
+
+class GeoSwitchAdmin {
+    private static $initialized = false;
+    private static $user_ip = null;
+    private static $record = null;
+	
+	public static function init() {
+        if (self::$initialized) {
+            return;
+        }
+        self::$initialized = true;
+        add_action('admin_menu', array('GeoSwitchAdmin', 'add_menu'));
+        add_action('admin_init', array('GeoSwitchAdmin', 'admin_init'));
+    }
+    
+    public static function admin_init() {    
+        register_setting( 'geoswitch_options', 'geoswitch_options', array('GeoSwitchAdmin', 'validate') );
+        add_settings_section('geoswitch_main', 'General Settings', array('GeoSwitchAdmin', 'main_section_text'), 'geoswitch_options_main_page');
+        add_settings_field('geoswitch_database_name', 'MaxMind Database Name', array('GeoSwitchAdmin', 'database_name'), 'geoswitch_options_main_page', 'geoswitch_main');
+        add_settings_field('geoswitch_units', 'Distance Units', array('GeoSwitchAdmin', 'units'), 'geoswitch_options_main_page', 'geoswitch_main');
+    }
+
+    public static function add_menu() {
+        add_options_page('GeoSwitch Plugin Options Page', 'GeoSwitch', 'manage_options', __FILE__, array('GeoSwitchAdmin', 'display_menu'));
+    }
+    
+    public static function display_menu() {
+?>
+<div>
+<h2>GeoSwitch Plugin Options</h2>
+<p>If you find this plugin useful why not</p>
+<a href='http://ko-fi.com?i=8d1dbd20374ff5c' target='_blank'><img style='border:0px' src='http://ko-fi.com/img/button-1.png' border='0' alt='Buy Me A Coffee :) @ ko-fi.com' /></a>
+
+<form method="post" action="options.php">
+<?php 
+    settings_fields('geoswitch_options');
+    do_settings_sections('geoswitch_options_main_page'); 
+?>
+<input name="Submit" type="submit" value="<?php esc_attr_e('Save Changes'); ?>" />
+</form>
+<?php
+    }
+
+    public static function main_section_text() {
+    }
+    
+    public static function database_name() {
+        $options = get_option('geoswitch_options');
+?>
+<input id='geoswitch_database_name' name='geoswitch_options[database_name]' size='64' type='text' value='<?= $options['database_name']?>' />
+<?php
+    }
+
+    public static function units() {
+        $options = get_option('geoswitch_options');
+?>
+<select id='geoswitch_units' name='geoswitch_options[units]'>
+  <option value="km" <?=selected($options['units'], 'km', false)?>>Kilometers</option>
+  <option value="m" <?=selected($options['units'], 'm', false)?>>Miles</option>
+</select>
+<?php
+    }
+
+    public static function validate($input)
+    {
+        if (isset($input['database_name'])) {
+            $newinput['database_name'] = trim($input['database_name']);
+            if(!preg_match('/^[a-z0-9._]+/i', $newinput['database_name'])) {
+                $newinput['database_name'] = 'GeoLite2-City.mmdb';
+            }
+        } else {
+            $newinput['database_name'] = 'GeoLite2-City.mmdb';
+        }
+            
+        if (isset($input['units'])) {
+            $newinput['units'] = ($input['units'] == 'm' ? 'm' : 'km');
+            
+        } else {
+            $newinput['units'] = 'km';
+        }
+        return $newinput;
+    }
+}
